ff2e02a4f6f1f0c497f53f160cd69b53fcba3553
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-12 11:37:00 -0700

    feat: :tada: init
    - Create new SvelteKit project with Tailwind CSS with `sv create`
    - Import designs, assets, and starter text from challange starter

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Frontend Mentor | Article preview component</title>
+</svelte:head>
+
+Shift the overall look and feel by adding these wonderful touches to furniture in your home Ever
+been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting.
+I&apos;ve got some simple tips to help you make any room feel complete. Michelle Appleton 28 Jun
+2020
